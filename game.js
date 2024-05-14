c6dcd647a79ea924b912b01a5fcf4b1c50ea83c3
Paul OHL <paul.lv.ohl@gmail.com>
2024-05-14 12:23:58 +0200

    feat: Wrote the JS to add words

---
+++ game.js
@@ -0,0 +1,46 @@
+const answers = document.getElementById('words');
+const wordInput = document.getElementById('word-input');
+
+const createWord = (word, proximity) => {
+  // Create the word element div
+  const wordElement = document.createElement('div');
+  wordElement.classList.add('rounded-xl', 'w-full', 'my-2', 'px-3', 'py-1');
+  if (proximity >= 90) {
+    wordElement.classList.add('bg-green-200');
+  } else {
+    wordElement.classList.add('bg-gray-400');
+  }
+
+  // Create the word text element
+  const wordText = document.createElement('p');
+  wordText.classList.add('font-bold', 'text-xl');
+  wordText.textContent = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  wordElement.appendChild(wordText);
+
+  // Create the proximity percentage
+  const proximityElement = document.createElement('p');
+  proximityElement.classList.add('font-semibold', 'text-md', 'text-gray-800');
+  proximityElement.textContent = `${proximity}%`;
+  wordElement.appendChild(proximityElement);
+
+  return wordElement;
+}
+
+const addWord = (word) => {
+  const proximity = Math.floor(Math.random() * 100) + 1;
+  const newWord = createWord(word, proximity);
+
+  // Hide the initial message
+  const initialMessage = document.getElementById('initial-message');
+  initialMessage.classList.add('hidden');
+
+  answers.prepend(newWord);
+
+  // Clear the input
+  wordInput.value = '';
+}
+
+document.getElementById('game-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  addWord(wordInput.value);
+});
