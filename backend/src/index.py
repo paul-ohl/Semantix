a29d550c588ae2c661fdaa5df72bf6eff4e24574
Paul OHL <paul.lv.ohl@gmail.com>
2024-05-16 14:07:27 +0200

    feat: Added getting the initial word to the Backend

--- backend/src/index.py
+++ backend/src/index.py
@@ -1,16 +1,22 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 import spacy
+import random
 
 nlp_fr = spacy.load('fr_core_news_md')
 nlp_en = spacy.load('en_core_web_md')
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route('/compare', methods=['POST'])
+@app.route('/api/compare', methods=['POST'])
+@cross_origin()
 def compare_words():
-    lang = request.get_json()['lang']
-    word1 = request.get_json()['word1']
-    word2 = request.get_json()['word2']
+    json_data = request.get_json()
+    lang = json_data['lang']
+    word1 = json_data['word1']
+    word2 = json_data['word2']
 
     nlp = nlp_fr if lang == 'fr' else nlp_en
 
@@ -19,3 +25,13 @@ def compare_words():
     similarity = token1.similarity(token2)
     print("Similarity:", similarity)
     return str(similarity), 200
+
+
+@app.route('/api/random_word', methods=['POST'])
+@cross_origin()
+def get_random_word():
+    lang = request.get_json()['lang']
+    file_path = 'dictionaries/dictionary.' + lang + '.txt'
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    return random.choice(lines)
